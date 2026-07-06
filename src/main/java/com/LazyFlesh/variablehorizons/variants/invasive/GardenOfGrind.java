d3edb364c23397ef545c4b04d8273fd752f4fabd
LazyFlesh <40587985+LazyFlesh@users.noreply.github.com>
2026-07-05 22:56:01 -0700

    forgot, but we do want to turn this off for gog. Skyblock, too, probably, when i get around to it

--- src/main/java/com/LazyFlesh/variablehorizons/variants/invasive/GardenOfGrind.java
+++ src/main/java/com/LazyFlesh/variablehorizons/variants/invasive/GardenOfGrind.java
@@ -3,6 +3,9 @@ package com.LazyFlesh.variablehorizons.variants.invasive;
 import com.LazyFlesh.variablehorizons.variants.VariantLoader;
 import com.LazyFlesh.variablehorizons.variants.VariantNames;
 
+import gregtech.GTMod;
+import gregtech.common.config.Worldgen;
+
 public class GardenOfGrind extends VariantLoader {
 
     public GardenOfGrind() {}
@@ -11,5 +14,11 @@ public class GardenOfGrind extends VariantLoader {
     public void loadVariant(VariantNames... activeVariants) {
         // gog doesn't need anything loaded (early mixins load on restart)
         VariantNames.GARDEN_OF_GRIND.hasLoaded = true;
+
+        // but it does need to overwrite some things
+        // the hodgepodge mixins have been ported here, because early mixins are hard to toggle config on.
+        Worldgen.endAsteroids.generateEndAsteroids = false;
+        // disable entity cramming (makes mob farms behave better)
+        GTMod.proxy.mMaxEqualEntitiesAtOneSpot = -1;
     }
 }
